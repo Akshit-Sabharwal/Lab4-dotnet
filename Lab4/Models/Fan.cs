5cd7c82c42bd205ab654fb1779bd4b084a565406
Akshit Sabharwal <sabh0009@algonquinlive.com>
2023-10-23 20:09:06 -0400

    Add project files.

---
+++ Lab4/Models/Fan.cs
@@ -0,0 +1,41 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace Lab4.Models
+{
+    public class Fan
+    {
+        public int ID { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)] 
+        [Display(Name = "Birth Date")]
+        public DateTime BirthDate { get; set; }
+
+        [Required]
+        public License License { get; set; }
+           
+
+        [Display(Name = "Name")]
+        public string FullName
+        {
+            get
+            {
+                return LastName + ", " + FirstName;
+            }
+        }
+
+       
+    }
+}
---
+++ Lab4/Models/SportClub.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Lab4.Models
+{
+    public class SportClub
+    {
+
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        [Display(Name = "Registration Number")]
+        [Required]
+        public string ID { get; set; }
+
+
+        [StringLength(50, MinimumLength = 1)]
+        [Required]
+        public string Title { get; set; }
+
+        [DataType(DataType.Currency)]
+        [Column(TypeName = "money")]
+        public decimal Fee { get; set; }
+
+    }
+}
